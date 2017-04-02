aa1fac413a0d1550387de32a79843ccfd49f71c9
Marchete <marchena.carlos@gmail.com>
2017-04-02 05:10:32 -0500

    Test Runner

--- projects/regexcourse/Validator.cs
+++ projects/regexcourse/Validator.cs
@@ -1,30 +1,31 @@
-using Answer;
-using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Text.RegularExpressions;
 
-namespace CodinGame
+namespace RegexCourse
 {
     [TestClass]
     public class Validator
     {
-	    public void VerifyMatches(Match ValidatorMatch,Match UserMatch)
-		{
-           Assert.AreEqual( UserMatch.Success, ValidatorMatch.Success);
-   	       if (ValidatorMatch.Success)
-			{
-			 //TODO: Check indexes and such
-			}
-		}
+        public void VerifyMatches(Match ValidatorMatch, Match UserMatch)
+        {
+            Assert.AreEqual(UserMatch.Success, ValidatorMatch.Success);
+            if (ValidatorMatch.Success)
+            {
+                //TODO: Check indexes and such
+            }
+        }
+
         [TestMethod]
-        public void VerifyExercise1() {
-		    List<string> INPUTS = new List<string>{"ABCDEFGHIJKLMNOPQRSTUVWXYXZ+abcdefeghijklmnopqrstuvwxyz","El veloz murcielago. Abria el paso hacia el siguiente arbol.","DDDDRTTTTTTPQRWT"};
-            string Pattern=@"[aeiouAEIOU]";
+        public void VerifyExercise1()
+        {
+            List<string> INPUTS = new List<string> { "ABCDEFGHIJKLMNOPQRSTUVWXYXZ+abcdefeghijklmnopqrstuvwxyz", "El veloz murcielago. Abria el paso hacia el siguiente arbol.", "DDDDRTTTTTTPQRWT" };
+            string Pattern = @"[aeiouAEIOU]";
             Regex regex = new Regex(Pattern);
-            foreach (string text in INPUTS){
-			  VerifyMatches(regex.Match(text),Exercise1.MatchVowels(text));
-			  }
+            foreach (string text in INPUTS)
+            {
+                VerifyMatches(regex.Match(text), Exercise1.MatchVowels(text));
+            }
         }
     }
 }
