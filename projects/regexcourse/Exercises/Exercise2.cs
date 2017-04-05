90a62c64acd2f7665f5edbcbbb7df8d7e13991ce
Marchete <marchena.carlos@gmail.com>
2017-04-05 11:30:02 -0500

    Intro with example

--- projects/regexcourse/Exercises/Exercise2.cs
+++ projects/regexcourse/Exercises/Exercise2.cs
@@ -1,8 +1,8 @@
 namespace RegexCourse{
-    public static class Exercise3{
+    public static class Exercise2{
 		//Write a regex pattern to match years from 2000 to 2199, both included.
 		//Remember that 20000 or 02000 won't be valid years, you need to limit 
 		// the size of the search to avoid more than 4 digits.
-        public static string Pattern_Exercise3=@"";
+        public static string Pattern_Exercise2=@"";
     }
 }
