2a7686f48a3a6766a26f881f6232313aafa95014
Marchete <marchena.carlos@gmail.com>
2017-06-22 14:06:45 -0500

    Fixing report.html

--- projects/regexcourse/Validator.cs
+++ projects/regexcourse/Validator.cs
@@ -328,11 +328,11 @@ string xml2 =
     public static class Exercise5{
 		//Search image files.
 		//Files are composed as follows:
-		//<filename>.<extension>
-		//<filename> is formed as one or more of the following characters:
+		//""filename.extension""
+		//""filename"" is formed as one or more of the following characters:
 		//    -Any alphanumeric character (a to z, and 0 to 9)
 		//    -Any of these symbols: .+-_=()
-		//Valid <extension> for images are:
+		//Valid ""extension"" for images are:
 		// jpg,jpeg,png,bmp,gif
 		
 		//The filesystem is case insensitive, so name and extension can be on any case.
