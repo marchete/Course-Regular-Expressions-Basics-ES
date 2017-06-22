fb63c87eb81c4c6ac00d2aa02af610d2f0c1d369
Marchete <marchena.carlos@gmail.com>
2017-06-22 13:56:29 -0500

    Fixing report.html

--- projects/regexcourse/Validator.cs
+++ projects/regexcourse/Validator.cs
@@ -161,8 +161,8 @@ namespace RegexCourse
 			File.WriteAllText (path, contents);
             Console.WriteLine("CG> message Report is:" + path + " Size:" + new System.IO.FileInfo(path).Length);
             Console.WriteLine("CG> message -channel \"exercise results\" Solved: " + countCorrect + "/" + regexcases.Count + " (" + percentage + "%)");
-            Console.WriteLine("CG> open --static-dir "+ReportPath+" /" + ReportName);
-			Console.WriteLine(contents);
+			Console.WriteLine("CG> message "contents);
+            Console.WriteLine("CG> open --static-dir "+ReportPath+" /" + ReportName);			
 			return UnitTestOK;
         }
 
